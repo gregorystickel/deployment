9ad590f7cd2cca0466ce1afcb97d316cc0b16b76
Greg Stickel <gregory.stickel@live.com>
2022-10-19 16:47:25 -0400

    Update 4:47pm

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const cors = require('cors')
-const port= process.env.PORT || 4000 
+const port = process.env.PORT || 4000 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
 const {getHTML, getCSS, getJS} = require('./controller')
-app.use(express.static('public'))
+
 app.get('/', getHTML)
 app.get('/css', getCSS)
 app.get('/js', getJS)
